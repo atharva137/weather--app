defc76d15504fff79b086825867a3d66bd532d4e
atharva137 <atharvayawalkar1377@gmail.com>
2021-06-18 18:29:14 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const viewsPath = path.join(__dirname, "../templetes/views");
 const partialPath = path.join(__dirname, "../templetes/partials");
 
 const app = express();
+const port = process.env.PORT||3000;
 
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
@@ -96,6 +97,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("web sever hosted");
 });
